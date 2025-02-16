571d045743e19e42b64dba01d5d99dd668ec9799
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-02-16 23:14:01 +0300

    Update home.go

--- backend/handlers/home.go
+++ backend/handlers/home.go
@@ -3,12 +3,13 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("frontend/templates/index.html")
+	tmpl, err := template.ParseFiles(filepath.Join("frontend", "templates", "index.html"))
 	if err != nil {
-		http.Error(w, "Error download page", http.StatusInternalServerError)
+		http.Error(w, "Error loading page", http.StatusInternalServerError)
 		return
 	}
 	tmpl.Execute(w, nil)
