41e1610b6e83776313595dbd71a06afd8a48808b
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-02-19 22:43:55 +0300

    Create db.go

---
+++ backend/internal/db/db.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func InitDB() (*sql.DB, error) {
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+
+	if host == "" {
+		host = "localhost"
+	}
+	if port == "" {
+		port = "5432"
+	}
+	if user == "" {
+		user = "postgres"
+	}
+	if password == "" {
+		password = "yourpassword"
+	}
+	if dbname == "" {
+		dbname = "yourdb"
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		username TEXT NOT NULL,
+		email TEXT UNIQUE NOT NULL,
+		password_hash TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`)
+	if err != nil {
+		log.Fatal("Error creating the table:", err)
+	}
+
+	log.Println("Connection to the database is successful")
+	return db, nil
+}
