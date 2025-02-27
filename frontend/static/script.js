78ca696ebb74fc54039b611aa705cb8e4d444cbc
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-02-27 22:49:17 +0300

    Update and rename app.js to script.js

---
+++ frontend/static/script.js
@@ -0,0 +1,7 @@
+/* static/script.js */
+document.addEventListener("DOMContentLoaded", function() {
+    // Добавляем класс для запуска анимации fade-in
+    document.body.classList.add("loaded");
+
+    // Здесь можно добавить дополнительный JS-код для интерактивности
+});
